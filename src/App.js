c5bd449f0b03f47035e3ad0a5e5af70eec989869
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-05-03 03:01:53 +0900

    イベントやスタイルのosnta 扱いについて

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 // JSX記法で記述
 const App = () => {
+  const onClickButton = () => alert("");
+  // ③オブジェクトの変数を定義してプロパティを書きスタイルを当てていく
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      {/* ①直接オブジェクトにスタイルを当てていく */}
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      {/* ③オブジェクトの変数を定義してプロパティを書きスタイルを当てていく */}
+      <p style={contentStyle}>お元気ですか？</p>
+      {/* ②タグにイベント、{}内にで関数を書く */}
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
