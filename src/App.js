cbf93efd507c4a23029e792a3f1bee3d2f9b8be7
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-05-03 20:06:24 +0900

    useEffectについて

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 // JSX記法で記述
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  // これだとひたすらレンダリングしてしまいReactのよくあるエラー
+  // 　　　Error
+  // 　　　Too many re-renders.
+  // が発生してしまう
+  // if (num % 3 === 0){
+  //   setFaceShowFlag(true);
+  // }else{
+  //   setFaceShowFlag(false);
+  // }
+  // useEffectは最初の一回だけ読み込んでくれる（関心の分離）
+  // 第二引数の変数が変化したときだけ処理を走らせることが出来る（[num]の事）
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // useEffectでnumを監視するようセットしている。
+    // これにより
+  }, [num]);
   // ③オブジェクトの変数を定義してプロパティを書きスタイルを当てていく
   // const contentLedyStyle = {
   //   color: "pink",
@@ -15,7 +42,8 @@ const App = () => {
   // ⑥useStateは関数。
   // 上のimport Reactに自動で補完され、{ useState }が入力される
   // [stateとして使用する変数名, 変数を変更するための関数（setOOOとするのが一般的）]
-  const [num, setNum] = useState(0);
+
+  //　表示・非表示を扱うstateを作成
   return (
     <>
       {/* ①直接オブジェクトにスタイルを当てていく */}
@@ -33,7 +61,10 @@ const App = () => {
       {/* <ColorfullMessage color="pink" message="元気です！"/> */}
 
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^_-)-☆</p>}
     </>
   );
 };
